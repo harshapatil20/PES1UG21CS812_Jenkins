cfb53e240546e5af69983410d45dd6208f7e788f
Your Nameharshapatil20 <harshapatil.karate2001@gmail.com>
2023-02-16 20:40:37 +0530

    webhook completed

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "1. Webhook added to your GitHub repository. 2. Console Output of second job displaying the change made in hello.cpp file.";
+  cout << "1. Webhook added to your GitHub repository and Console Output of second job displaying the change made in hello.cpp file.";
   return 0;
 }
